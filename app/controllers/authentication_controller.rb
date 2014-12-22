a3b23eadc3258f964ead34052201d8522bc826eb
Max Gordon <gordonmaxc@gmail.com>
2014-12-22 13:30:55 -0800

    user signin

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,4 +7,16 @@ class AuthenticationController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to signin_path, alert:"Username / password combination is invalid"
+    end
+  end
+
 end
