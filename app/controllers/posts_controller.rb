f13623e7cde4de6b69f14c10afc08b260fdb278c
Max Gordon <gordonmaxc@gmail.com>
2014-12-22 14:54:13 -0800

    added edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -16,8 +21,17 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
+  def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post updated succesfully"
+    else
+      render :edit
+    end
   end
 
   private
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -12,5 +12,30 @@ feature PostsController do
     expect(page).to have_link("post-edit-action")
   end
 
+  scenario "#edit" do
+    test_post = Post.create!(title:"Testing Post", description:"Testing Description")
 
+    visit root_path
+    click_on "post-show-action"
+    click_on "post-edit-action"
+    #login info after authorization
+    fill_in :post_title, with:"New Title"
+    fill_in :post_description, with:"New Description"
+    click_on "post-create-action"
+
+    expect(page).to have_link("post-edit-action")
+    expect(page.current_path).to eq(post_path(test_post))
+  end
+
+  scenario "#delete" do
+    test_post = Post.create!(title:"Testing Post", description:"Testing Description")
+
+    visit root_path
+    click_on "post-show-action"
+    click_on "post-delete-action"
+    #login info after authorization
+
+    expect(page).to have_no_link(test_post.title)
+    expect(page.current_path).to eq(root_path)
+  end
 end
