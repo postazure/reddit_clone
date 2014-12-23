b864a0ae6f50355ed835bb772f08f302b2727475
Max Gordon <gordonmaxc@gmail.com>
2014-12-22 23:35:42 -0800

    fixed resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "posts#index"
 
   resources :posts do
-    resources :comments
+    resources :comments, only: [:new, :create]
   end
 
   get   "/signup" => "registrations#new"
