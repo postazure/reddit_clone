7ae0d36d1a3b4fd4cf695b6f468b958450a0f010
Max Gordon <gordonmaxc@gmail.com>
2014-12-22 23:16:07 -0800

    order posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(updated_at: :desc)
   end
 
   def show
