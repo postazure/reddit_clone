f3fec9b74dbda6445bc19456b43d530caf958480
Max Gordon <gordonmaxc@gmail.com>
2014-12-23 11:02:18 -0800

    add user view

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,21 +1,23 @@
 require "rails_helper"
 
 describe User do
-  describe "navigate from " do
-    scenario "post show"
-
-    scenario "comment show"
-
-    scenario "navbar"
-  end
-
   describe "show view" do
     scenario "has elements" do
-      expect(page).to have_content("user-username")
-      expect(page).to have_content("user-post-count")
-      expect(page).to have_content("user-lastpost-timer")
-      expect(page).to have_content("user-comment-count")
-      expect(page).to have_content("user-lastcomment-timer")
+      test_user = User.create!(email:"g@g.com", username:"max", password:"123", password_confirmation:"123" )
+      test_post = Post.create!(title:"Testing Post", description:"Testing Description", user_id:test_user.id)
+      visit root_path
+      click_on "signin-action"
+
+      fill_in :email, with: "g@g.com"
+      fill_in :password, with: "123"
+      click_on "user-login-action"
+      #---------------------------
+
+      visit root_path
+      click_on "user-show-action"
+
+      expect(page.current_path).to have_content(user_path(test_user))
+      expect(page).to have_content("max")
     end
 
   end
